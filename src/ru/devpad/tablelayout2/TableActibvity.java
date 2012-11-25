298b6392a2ca42d29d595e71b42f737c87c78a41
wapplay <wapplay86@gmail.com>
2012-11-25 15:54:07 +0400

    example app for devpad.ru "Overview TableLayout -2"

---
+++ src/ru/devpad/tablelayout2/TableActibvity.java
@@ -0,0 +1,54 @@
+package ru.devpad.tablelayout2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.*;
+
+public class TableActibvity extends Activity {
+    private TableLayout table;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.table_ex3);
+
+        setTitle("Example #3 - программное добавление строк");
+
+        Button buttonAdd = (Button) findViewById(R.id.add_button);
+        table = (TableLayout) findViewById(R.id.table);
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                EditText addUser = (EditText) findViewById(R.id.add_user);
+                EditText addPoint = (EditText) findViewById(R.id.add_point);
+
+                String addUserValue = addUser.getText().toString();
+                String addPointValue = addPoint.getText().toString();
+
+                if (addUserValue.equals("") || addPointValue.equals("")) {
+                    Toast.makeText(getApplicationContext(), "Не заполнены все поля!", Toast.LENGTH_SHORT).show();
+                } else {
+                    TextView textUser = new TextView(getApplicationContext());
+                    textUser.setGravity(Gravity.CENTER_HORIZONTAL);
+                    textUser.setTextColor(0xff000000);
+                    textUser.setText(addUserValue);
+
+                    TextView textPoint = new TextView(getApplicationContext());
+                    textPoint.setGravity(Gravity.CENTER_HORIZONTAL);
+                    textPoint.setTextColor(0xff000000);
+                    textPoint.setText(addPointValue);
+
+                    TableRow row = new TableRow(getApplicationContext());
+                    row.addView(textUser);
+                    row.addView(textPoint);
+
+                    addUser.setText("");
+                    addPoint.setText("");
+
+                    table.addView(row);
+                }
+            }
+        });
+    }
+}
